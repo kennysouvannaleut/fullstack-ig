b2b129f60c90c038e3f8db7258ccda5975b23c41
Evan Taylor <evantaylor667@gmail.com>
2020-03-10 14:10:19 -0600

    added date to each post

--- routes/postNew.js
+++ routes/postNew.js
@@ -4,6 +4,7 @@ const Post = require('../models/post.js')
 
 postNew.post('/', (req, res, next) => {
     const newPost = new Post(req.body)
+    // const date = Date()
     newPost.save((err, post) => {
         if(err){
             res.status(500)

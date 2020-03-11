90dda5b0d3d4ea496533c4fd4ab5e0e0bee06441
Kenny Souvannaleut <ksouvannaleut@programmer.net>
2020-03-11 13:34:35 -0600

    added userAuth

--- client/src/context/UserProvider.js
+++ client/src/context/UserProvider.js
@@ -3,34 +3,74 @@ import useFetch from '../hooks/useFetch';
 import axios from 'axios';
 
 export const UserContext = createContext();
+const userAxios = axios.create();
 
 const handleError = err => console.log(err.response.data.errMsg);
 
 const UserProvider = (props) => {
+    const initialState = {
+        user: JSON.parse(localStorage.getItem('user')) || {},
+        posts: []
+    };
+
+    const [userState, setUserState] = useState(initialState);
+    // const [query, setQuery] = useState('user');
+
     const [{ 
         data, 
         isLoading, 
         isError 
-    }, apiFetch] = useFetch('/view', { posts: [] } );
-
-    const [state, setState] = useState(data);
-    const [query, setQuery] = useState('user');
+    }, apiFetch] = useFetch('/viewposts');
 
     const getPosts = () => { 
         apiFetch()
         const { posts } =  data;
-        setState({
+        setUserState({
             isError,
             isLoading,
             posts
         })
     };
 
-    const getUserPost = (userId) => {
-        axios.get(`/post/${userId}`)
+    const signup = (credentials) => {
+        userAxios.post('auth/signup', credentials)
             .then(res => {
-                setState(prevState => ({
-                    ...prevState,
+                localStorage.setItem('user', JSON.stringify(user))
+                setUserState(prevUserState => ({
+                    ...prevUserState,
+                    user
+                }))
+            })
+            .catch(handleError)
+    };
+
+    const login = (credentials) => {
+        userAxios.post('auth/login', credentials)
+            .then(res => {
+                const { user } = res.data;
+                localStorage.setItem('user', JSON.stringify(user))
+                getPosts()
+                setUserState(prevUserState => ({
+                    ...prevUserState,
+                    user
+                }))
+            })
+            .catch(handleError)
+    };
+
+    const logout = () => {
+        localStorage.removeItem('user')
+        setUserState({
+            user: {},
+            posts: []
+        })
+    };
+
+    const getUserPost = (userId) => {
+        axios.get(`/users/${userId}`)
+            .then(res => {
+                setUserState(prevUserState => ({
+                    ...prevUserState,
                     posts: res.data
                 }))
             })
@@ -40,10 +80,10 @@ const UserProvider = (props) => {
     const createPost = (newPost) => {
         axios.post('/post', newPost)
             .then(res => {
-                setState(prevState => ({
-                    ...prevState,
+                setUserState(prevUserState => ({
+                    ...prevUserState,
                     posts: [ 
-                        ...prevState.posts,
+                        ...prevUserState.posts,
                         res.data
                     ]
                 }))
@@ -52,35 +92,38 @@ const UserProvider = (props) => {
     };
 
     const removePost = (postId) => {
-        axios.delete(`/post/${postId}`)
+        axios.delete(`/update/${postId}`)
             .then(() => {
-                setState(prevState => prevState.filter(post => post._id !== postId))
+                setUserState(prevUserState => prevUserState.filter(post => post._id !== postId))
             })
             .catch(handleError)
     };
 
-    const editPost = (edit, postId) => {
-       axios.put(`/post/${postId}`, edit)
+    const editPost = (update, postId) => {
+       axios.put(`/update/${postId}`, update)
         .then(res => {
-            setState(prevState => prevState.map(post => post._id !== postId ? post: res.data))
+            setUserState(prevUserState => prevUserState.map(post => post._id !== postId ? post: res.data))
         })
         .catch(handleError)
     };
 
-    const filterPost = (e) => {
-        if(e.target.value === 'reset') {
-            apiFetch()
-        } else {
-            axios.get(`/view/search?query=${query}`)
-                .then(res => setQuery(res.data))
-                .catch(handleError)
-        }
-    };
+    // const filterPost = (e) => {
+    //     if(e.target.value === 'reset') {
+    //         apiFetch()
+    //     } else {
+    //         axios.get(`/viewposts/search?query=${query}`)
+    //             .then(res => setQuery(res.data))
+    //             .catch(handleError)
+    //     }
+    // };
 
     return (
         <UserContext.Provider 
             value={{ 
-                ...state,
+                ...userState,
+                signup,
+                login,
+                logout,
                 getPosts,
                 getUserPost,
                 createPost,

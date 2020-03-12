ac6bbd261cfab66dc045899fbbe350b7a92468a4
Evan Taylor <evantaylor667@gmail.com>
2020-03-12 11:40:57 -0600

    backend

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 
-var firebaseConfig = {
-        apiKey: 'AIzaSyBMzNMMbyeAsrmfVwA0I4s4lGM4AR7jOq4',
-        authDomain: '<your-auth-domain>',
-        databaseURL: '<your-database-url>',
-        storageBucket: 'image-bucket-143c1.firebaseapp.com'
-    }
-    firebase.initializeApp(firebaseConfig)
+const firebaseConfig = {
+    apiKey: "AIzaSyBMzNMMbyeAsrmfVwA0I4s4lGM4AR7jOq4",
+    authDomain: "image-bucket-143c1.firebaseapp.com",
+    databaseURL: "https://image-bucket-143c1.firebaseio.com",
+    projectId: "image-bucket-143c1",
+    storageBucket: "image-bucket-143c1.appspot.com",
+    messagingSenderId: "396601837101",
+    appId: "1:396601837101:web:cfb369d92c08c6c9809aa4",
+    measurementId: "G-CK3LLXRTX5"
+  }
+
+firebase.initializeApp(firebaseConfig)
 
 const dbURL = 'mongodb://localhost:27017/group-api'
 

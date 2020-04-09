280aafb877e52ebfc7b71282e694a68e70433afa
Evan Taylor <evantaylor667@gmail.com>
2020-04-09 11:29:15 -0600

    backend

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -41,7 +41,7 @@ userAuth.post('/login', (req, res, next) => {
             res.status(403)
             return next(new Error('Username is incorrect'))
         }
-        user.comparePassword(req.body.password, (err, isMatch) => {
+        user.checkPassword(req.body.password, (err, isMatch) => {
             if (err) {
                 res.status(403);
                 return next(new Error('Username or password are incorrect'));
